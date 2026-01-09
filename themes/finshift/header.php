772fd83db21c14b0e810f57c7b9f410db22e3c33
AkiraMatsumoto-github <95224877+AkiraMatsumoto-github@users.noreply.github.com>
2026-01-09 23:23:26 +0900

    favicon

--- themes/finshift/header.php
+++ themes/finshift/header.php
@@ -11,7 +11,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.svg" type="image/svg+xml">
+
 
 	<?php wp_head(); ?>
 </head>
